0850a43bf620809d1ef2e96e99f9d25307e109c9
Rainy <browngres@outlook.com>
2024-07-11 19:30:14 +0800

    填加《标识符》，调用本地包实践

---
+++ go-learn/src/learn1/identifier/identifier.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "identifier/p1"
+
+func main (){
+	var a1 int = 666
+	fmt.Println(a1)
+	fmt.Println(p1.Num1_p1)
+}
---
+++ go-learn/src/learn1/identifier/p1/p1.go
@@ -0,0 +1,4 @@
+package p1
+
+var Num1_p1 int = 601
+var num2_p1 int = 602
