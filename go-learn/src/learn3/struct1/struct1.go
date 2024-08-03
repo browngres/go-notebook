a9a2f80f42bacefa4ecd701297729053ab5d5bda
Rainy <browngres@outlook.com>
2024-08-03 10:44:51 +0800

    Add：《结构体进阶》、代码 struct1

---
+++ go-learn/src/learn3/struct1/struct1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type student struct {
+	name string
+	age  int
+}
+
+func main() {
+	m := make(map[string]*student)
+	stus := []student{
+		{name: "name1", age: 11},
+		{name: "name2", age: 22},
+		{name: "name3", age: 33},
+	}
+
+	for _, stu := range stus {
+		m[stu.name] = &stu
+	}
+	for k, v := range m {
+		fmt.Println(k, "=>", v.name, v.age)
+	}
+}
